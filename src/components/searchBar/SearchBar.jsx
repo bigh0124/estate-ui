5559dfad06a3b19eeb4ee1f24f8def775635e891
bigh0124 <thubgi0629@gmail.com>
2024-05-08 17:24:09 +0800

    Feat: 完成hero section頁面和searchBar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const handleClick = (e) => {
+    setQuery((prev) => ({
+      ...prev,
+      type: e.target.name,
+    }));
+  };
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        <button name="buy" onClick={handleClick} className={query.type === "buy" ? "active" : ""}>
+          Buy
+        </button>
+        <button name="rent" onClick={handleClick} className={query.type === "rent" ? "active" : ""}>
+          Rent
+        </button>
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input type="number" name="minPrice" min={0} max={100000000} placeholder="Min Price" />
+        <input type="number" name="maxPrice" min={0} max={100000000} placeholder="Max Price" />
+        <button>
+          <img src="/search.png" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
