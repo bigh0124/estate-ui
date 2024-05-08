787c7076a3d93e57be413e0a292c7bcd21bddf72
bigh0124 <thubgi0629@gmail.com>
2024-05-08 14:24:10 +0800

    Feat: 新增漢堡選單

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav>
       <div className="left">
@@ -14,10 +17,21 @@ const Navbar = () => {
         <a href="/">Agents</a>
       </div>
       <div className="right">
-        <a href="/">Sign In</a>
+        <a href="/">Sign in</a>
         <a href="/" className="register">
-          Sign out
+          Sign up
         </a>
+        <div className="menuIcon">
+          <img src="/menu.png" onClick={() => setOpen((prev) => !prev)} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
