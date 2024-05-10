9c9771bab513ffea3c7335599dea28cc37b6411d
bigh0124 <thubgi0629@gmail.com>
2024-05-10 14:37:01 +0800

    Refactor: 修改a tag成Link tag

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -7,30 +8,30 @@ const Navbar = () => {
   return (
     <nav>
       <div className="left">
-        <a href="/" className="logo">
+        <Link to={"/"} className="logo">
           <img src="/logo.png" />
           <span>EstateM</span>
-        </a>
-        <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agents</a>
+        </Link>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
+        <Link to={"/agents"}>Agents</Link>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">
+        <Link to={"/login"}>Sign in</Link>
+        <Link to={"/register"} className="register">
           Sign up
-        </a>
+        </Link>
         <div className="menuIcon">
           <img src="/menu.png" onClick={() => setOpen((prev) => !prev)} />
         </div>
         <div className={open ? "menu active" : "menu"}>
-          <a href="/">Home</a>
-          <a href="/">About</a>
-          <a href="/">Contact</a>
-          <a href="/">Agents</a>
-          <a href="/">Sign in</a>
-          <a href="/">Sign up</a>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/contact"}>Contact</Link>
+          <Link to={"/agents"}>Agents</Link>
+          <Link to={"/login"}>Sign in</Link>
+          <Link to={"/register"}>Sign up</Link>
         </div>
       </div>
     </nav>
