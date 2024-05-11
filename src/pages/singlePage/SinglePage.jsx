2e6450ada24f72ca310b478e178d26529b8b5fcf
bigh0124 <thubgi0629@gmail.com>
2024-05-11 13:23:15 +0800

    Feat: 完成singlePage post user部分

--- src/pages/singlePage/SinglePage.jsx
+++ src/pages/singlePage/SinglePage.jsx
@@ -1,7 +1,39 @@
+import Slider from "../../components/slider/Slider";
 import "./singlePage.scss";
+import { singlePostData, userData } from "../../lib/dummyData";
 
 const SinglePage = () => {
-  return <div>SinglePage</div>;
+  const { images, title, address, description, price } = singlePostData;
+
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{title}</h1>
+                <p>
+                  <img src="/pin.png" />
+                  {address}
+                </p>
+                <span className="price">${price}</span>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
