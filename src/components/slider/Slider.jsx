a06ad9ce9c424b2c463f1ed9f726e63a1ee2d943
bigh0124 <thubgi0629@gmail.com>
2024-05-13 10:58:03 +0800

    Feat: 完成Slider component

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
 import "./slider.scss";
 
 const Slider = ({ images }) => {
-  return <div>Slider</div>;
+  const [firstImage, ...restImage] = images;
+  const [imageIndex, setImageIndex] = useState(null);
+  console.log(imageIndex);
+  const handleImageClick = (imageIndex) => {
+    setImageIndex(imageIndex);
+  };
+
+  const handleArrowClick = (imageIndex) => {
+    if (imageIndex < 0) {
+      return setImageIndex(images.length - 1);
+    }
+    if (imageIndex > images.length - 1) {
+      return setImageIndex(0);
+    }
+
+    setImageIndex(imageIndex);
+  };
+
+  return (
+    <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow">
+            <img src="/arrow.png" alt="" onClick={() => handleArrowClick(imageIndex - 1)} />
+          </div>
+          <div className="imageContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow">
+            <img src="/arrow.png" alt="" className="right" onClick={() => handleArrowClick(imageIndex + 1)} />
+          </div>
+          <div className="close">
+            <span onClick={() => handleImageClick(null)}>X</span>
+          </div>
+        </div>
+      )}
+      <div className="bigImage">
+        <img src={firstImage} alt="" onClick={() => handleImageClick(0)} />
+      </div>
+      <div className="smImages">
+        {restImage.map((img, index) => (
+          <img src={img} key={img} onClick={() => handleImageClick(index + 1)} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
