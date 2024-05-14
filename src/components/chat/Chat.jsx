9358520e84f043f56a78e808333bdd34abbdfe6b
bigh0124 <thubgi0629@gmail.com>
2024-05-14 11:45:22 +0800

    Feat: 完成Chat component

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,54 @@
+import "./chat.scss";
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      <h1>Messages</h1>
+      <div className="messages">
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="img"
+          />
+          <span>John</span>
+          <p>Lorem ipsum dolor sit amet ...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="img"
+          />
+          <span>John</span>
+          <p>Lorem ipsum dolor sit amet ...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="img"
+          />
+          <span>John</span>
+          <p>Lorem ipsum dolor sit amet ...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="img"
+          />
+          <span>John</span>
+          <p>Lorem ipsum dolor sit amet ...</p>
+        </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="img"
+          />
+          <span>John</span>
+          <p>Lorem ipsum dolor sit amet ...</p>
+        </div>
+      </div>
+      <div className="chatBox"></div>
+    </div>
+  );
+};
+
+export default Chat;
